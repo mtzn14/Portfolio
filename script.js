d2a5b104658b73e9da77749fba3e6f6998f5dfc7
mtzn14 <zunicmatej@gmail.com>
2024-11-12 00:01:54 +0100

    updating

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-.slideshow {
-    position: relative;
-    width: 100%;  /* Set your desired width */
-    max-width: 400px;  /* Adjust the max width as needed */
-    height: 300px;  /* Set your desired height */
-    overflow: hidden;
-}
 
-.slideshow img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    display: none;  /* Hide all images by default */
-}
